e0558aa77166984ac278f673737ad449fe1afb21
ducthuanle <ducthuan.le1501@gmail.com>
2025-08-12 13:17:46 +0200

    implement the user-service modul

--- backend/user-service/src/app.module.ts
+++ backend/user-service/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: +(process.env.PG_PORT ?? 5432), // Default to 5432 if PG_PORT is undefined
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class AppModule {}
